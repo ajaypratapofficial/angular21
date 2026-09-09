44600a1cafd15bc6f6144b198b51a890e9ca0379
Ajay Pratap SIngh <127107850+ajaypratapofficial@users.noreply.github.com>
2026-09-09 17:48:50 +0530

    child to parent - output - emit

--- src/app/child/child.ts
+++ src/app/child/child.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  OnChanges,
+  SimpleChanges,
+} from "@angular/core";
 
 interface User {
   name: string;
@@ -6,16 +13,33 @@ interface User {
 }
 
 @Component({
- selector: 'app-child',
- imports: [],
- templateUrl: './child.html',
- styleUrl: './child.css'
+  selector: "app-child",
+  imports: [],
+  templateUrl: "./child.html",
+  styleUrls: ["./child.css"],
 })
 export class Child implements OnChanges {
-
+  // Parent → Child
   @Input() user!: User;
 
+  // Child → Parent
+  @Output() userChanged = new EventEmitter<User>();
+
   ngOnChanges(changes: SimpleChanges) {
-    console.log(changes['user']);
+    console.log(changes["user"]);
+  }
+
+  changeNameFromChild() {
+    const updatedUser = {
+      ...this.user,
+      name: "ChildRahul",
+    };
+
+    this.userChanged.emit(updatedUser);
+    // Child
+    //   │
+    //   │ emit(updatedUser)
+    //   ▼
+    // Parent
   }
 }
--- src/app/parent/parent.ts
+++ src/app/parent/parent.ts
@@ -1,21 +1,44 @@
-import { Component } from '@angular/core';
-import { Child } from '../child/child';
+import { Component } from "@angular/core";
+import { Child } from "../child/child";
+
+interface User {
+  name: string;
+  age: number;
+}
 
 @Component({
-  selector: 'app-parent',
+  selector: "app-parent",
   imports: [Child],
-  templateUrl: './parent.html',
-  styleUrl: './parent.css',
+  templateUrl: "./parent.html",
+  styleUrls: ["./parent.css"],
 })
 export class Parent {
-
-  user = {
-    name: 'Ajay',
-    age: 31
+  user: User = {
+    name: "Ajay",
+    age: 31,
   };
 
   changeName() {
-    this.user.name = 'Rahul';
+    // this.user.name = "ParentRahul";
+    this.user = {
+      ...this.user,
+      name: "ParentRahul",
+    };
   }
- 
+
+  onUserChanged(updatedUser: User) {
+    this.user = updatedUser;
+  }
+  // Child
+  //  │
+  //  │ this.userChanged.emit(updatedUser)
+  //  │
+  //  ▼
+  // (userChanged)
+  //  │
+  //  ▼
+  // onUserChanged($event)
+  //  │
+  //  ▼
+  // Parent user
 }
