f7e5be4300f310c1db5f32b73b7119b2fba13e28
Ajay Pratap SIngh <127107850+ajaypratapofficial@users.noreply.github.com>
2026-09-10 12:11:23 +0530

    view child - with input element

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   imports: [],
-  templateUrl: './app.html',
-  styleUrl: './app.css',
+  templateUrl: "./app.html",
+  styleUrl: "./app.css",
 })
-export class App {
+export class App implements AfterViewInit {
+  @ViewChild("myInput")
+  input!: ElementRef<HTMLInputElement>;
+
+  // The HTML element doesn't exist when the component class is initially constructed.
+  ngAfterViewInit() {
+    console.log(this.input);
+  }
+
+  focusInput() {
+    this.input.nativeElement.focus();
+    // this.input.nativeElement gives you actual <input> element
+    // this.input.nativeElement.focus() -> calls the browser's focus() method 
+  }
+
+  clearInput() {
+    this.input.nativeElement.value = "";
+  }
 
 }
