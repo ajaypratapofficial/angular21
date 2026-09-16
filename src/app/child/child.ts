40f4107ad08b3bf9e6d9b6ac83cb69c46268af20
Ajay Pratap SIngh <127107850+ajaypratapofficial@users.noreply.github.com>
2026-09-16 12:00:56 +0530

    parent to child - simple string - transfer

---
+++ src/app/child/child.ts
@@ -0,0 +1,22 @@
+import { Component, Input, SimpleChanges } from "@angular/core";
+
+@Component({
+  selector: "app-child",
+  imports: [],
+  templateUrl: "./child.html",
+  styleUrl: "./child.css",
+})
+export class Child {
+  @Input() name!: string;
+
+  // Angular automatically detects changes in input properties, so ngOnChanges is not strictly necessary unless you need to perform additional actions when the input changes.
+  // If you do need to perform additional actions when the input changes, you can implement the ngOnChanges lifecycle hook.
+  // Example of using ngOnChanges:
+
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes["name"]) {
+      console.log("Name changed to:", changes["name"].currentValue);
+    }
+  }
+}
+
---
+++ src/app/parent/parent.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+import { Child } from "../child/child";
+
+@Component({
+  selector: "app-parent",
+  imports: [Child],
+  templateUrl: "./parent.html",
+  styleUrl: "./parent.css",
+})
+export class Parent {
+  username = "ajay";
+  changeUsername() {
+    this.username = "rahul";
+  }
+}
