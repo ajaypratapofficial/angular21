6800fcfa80eac0153b0c5605f930c99724488d8a
Ajay Pratap SIngh <127107850+ajaypratapofficial@users.noreply.github.com>
2026-09-18 19:31:15 +0530

    Template-driven Form Validation

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,12 @@ import { FormsModule } from "@angular/forms";
 })
 export class App {
   name = "";
+  email = "";
+  password = "";
+
+  submitForm() {
+    console.log("Name:", this.name);
+    console.log("Email:", this.email);
+    console.log("Password:", this.password);
+  }
 }
