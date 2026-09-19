5673276ac466e04333f40434e688a2c459cbaf98
Ajay Pratap SIngh <127107850+ajaypratapofficial@users.noreply.github.com>
2026-09-19 09:54:00 +0530

    FormGroup | FormControl  | formControlName |

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,20 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  imports: [],
-  templateUrl: './app.html',
-  styleUrl: './app.css',
+  selector: "app-root",
+  imports: [ReactiveFormsModule],
+  templateUrl: "./app.html",
+  styleUrls: ["./app.css"],
 })
 export class App {
+  form = new FormGroup({
+    name: new FormControl(""),
+    email: new FormControl(""),
+    password: new FormControl(""),
+  });
 
+  submitForm() {
+    console.log(this.form.value);
+  }
 }
