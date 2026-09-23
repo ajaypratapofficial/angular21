a6bc73d05a06ce0992bf43aec797c25713948aad
Ajay Pratap SIngh <127107850+ajaypratapofficial@users.noreply.github.com>
2026-09-23 11:33:17 +0530

    RxJS Observable

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
+  standalone: true,
   imports: [],
-  templateUrl: './app.html',
-  styleUrl: './app.css',
+  templateUrl: "./app.html",
+  styleUrls: ["./app.css"],
 })
 export class App {
+  numbers: number[] = [];
 
+  status = "Not Started";
+
+  // Create Observable
+  numbers$ = new Observable<number>((subscriber) => {
+    subscriber.next(10);
+    subscriber.next(20);
+    subscriber.next(30);
+
+    subscriber.complete();
+  });
+
+  // Subscribe to Observable
+  runObservable() {
+    this.numbers = [];
+    this.status = "Running";
+
+    this.numbers$.subscribe({
+      next: (value) => {
+        this.numbers.push(value);
+      },
+
+      error: (error) => {
+        this.status = "Error";
+        console.error(error);
+      },
+
+      complete: () => {
+        this.status = "Completed";
+      },
+    });
+  }
 }
